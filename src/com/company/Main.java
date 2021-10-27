5ce83b15f14ebb262ee2f50a4e3fb5bf0f718d30
User <selivanova.sarra@gmail.com>
2021-10-27 13:03:39 +0300

    ggfhgfghfh

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter year to chak:");
+        int year=scanner.nextInt();
+        if(year %4==0)
+            System.out.println("thus is a leep year\n29 days in feb");
+        else
+            System.out.println("regular year 28 days");
+    }
+}
