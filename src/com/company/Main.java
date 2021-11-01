ecfd828ad594722214efdf24f5d9b713ec6d7e90
User <selivanova.sarra@gmail.com>
2021-11-01 17:59:00 +0200

    שיעורי בית

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner=new Scanner(System.in);
         System.out.println("enter year to chak:");
         int year=scanner.nextInt();
