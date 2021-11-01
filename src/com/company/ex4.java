86f6c55fdbb8c4ca692d25eb562cdea50f271912
User <selivanova.sarra@gmail.com>
2021-11-01 17:56:18 +0200

    ggfhgfghfh

--- src/com/company/ex4.java
+++ src/com/company/ex4.java
@@ -1,5 +1,12 @@
 package com.company;
-
+import java.util.Scanner;
 public class ex4 {
-    //bla bla bla
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        if (num % 2 != 0)
+            System.out.println("odd value");
+        else
+            System.out.println("even value");
+    }
 }
